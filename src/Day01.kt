2eb961da26f6a877f30868474dbb021fb3f3bf31
Lukasz Kalnik <lukasz.kalnik@gmail.com>
2024-12-13 19:50:33 +0100

    Day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,20 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val idsLineRegex = """(\d+) +(\d+)""".toRegex()
+        val (ids0, ids1) = input.map {
+            val matchGroups = idsLineRegex.matchEntire(it)!!.groupValues
+            matchGroups[1].toInt() to matchGroups[2].toInt()
+        }.unzip()
+
+        val sumOfDiffs = ids0.sorted().zip(ids1.sorted()).sumOf { (id0, id1) -> abs(id0 - id1) }
+        return sumOfDiffs
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
